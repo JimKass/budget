94804a83294c6ad5c76e1140466822de4e8bdebd
JimKass <james.kastner@my.jcu.edu.au>
2018-01-22 12:25:41 +1000

    Added get-payslips

--- src/budget/core.clj
+++ src/budget/core.clj
@@ -1,13 +1,22 @@
-(ns budget.core)
+(ns budget.core
+  (require [clojure-mail.core :refer :all]
+           [clojure-mail.gmail :as gmail]
+           [clojure-mail.message :as message]))
 
 (defn read-payslip
   [filename]
-  (let [pdf-code (slurp filename)
-        payout-flag "<TD CLASS=\"netPayDetail\" VALIGN=\"top\" align=\"right\">$</TD><TD CLASS=\"netPayDetail\" align=\"right\">"
-        re-finder (re-pattern (str "(?=" payout-flag ").+?(?<=/<//TD/>)"))
-        payout (re-find re-finder pdf-code)]
-    payout))
+  (let [htm-code (slurp filename)
+        payout-flag "<TD CLASS=\"netPayDetail\" VALIGN=\"top\" align=\"right\">\\$<\\/TD><TD CLASS=\"netPayDetail\" align=\"right\">"
+        re-finder (re-pattern (str "(?<=" payout-flag ").+?(?=<\\/TD>)"))
+        payout (re-find re-finder htm-code)]
+    (Float/parseFloat payout)))
+
+(defn get-payslips
+  [user pswd]
+  (let [store (store "imap.gmail.com" user pswd)
+        payslips (search-inbox store [:from "payroll@ardentleisure.com"])]
+    payslips))
 
 (defn -main
   []
-  (read-payslip))
+  (println "I do nothing right now."))
--- test/budget/core_test.clj
+++ test/budget/core_test.clj
@@ -1,17 +1,31 @@
 (ns budget.core-test
   (:require [clojure.test :refer :all]
             [budget.core :refer :all])
-  (:import (java.io FileNotFoundException)))
+  (:import (java.io FileNotFoundException)
+           (java.util.regex Pattern)))
+
+(defn re-quoted [s]
+  (re-pattern (Pattern/quote s)))
+
+(defn float=
+  [x y]
+  (<= (Math/abs ^float (- x y)) 0.01))
 
 (deftest read-payslip-test
   (testing "is a function"
     (is (fn? read-payslip)))
 
   (testing "takes a file and returns and integer"
-    (is (= 225.54 (read-payslip "src/budget/PAYSLIP191217.htm"))))
+    (is (float= 225.54 (read-payslip "src/budget/PAYSLIP191217.htm"))))
 
   (testing "will throw an exception if file is not exist"
     (is (thrown-with-msg?
           java.io.FileNotFoundException
-          #"src/budget/PAYSLIP191217.html (No such file or directory)"
+          ( re-quoted "src/budget/PAYSLIP191217.html (No such file or directory)")
           (read-payslip "src/budget/PAYSLIP191217.html")))))
+
+(deftest get-payslips-test
+  (testing "is a function"
+    (is (fn? get-payslips))))
+
+; TODO: Make up more tests for get-payslips
