71bada38160ebd0014fd5b2d5da83f2fd46f0389
JimKass <james.kastner@my.jcu.edu.au>
2018-03-29 19:10:30 +1000

    Added some Usage stuff

--- budget.sh
+++ budget.sh
@@ -6,35 +6,66 @@ ${txtbld}SYNOPSIS${txtrst}
     $0 is used to keep track of payslip, payments and estimate the amount of pay will be received.
 
 ${txtbld}USAGE${txtrst}
-    ./budget <OPTION> <PARAMS>
+    ./budget <OPTIONS> COMMAND <PARAMS>
 
-${txtbld}OPTIONS AND PARAMS${txtrst}
-    payslip
+${txtbld}COMMANDS${txtrst}
+    help            prints usage of specified command. Prints this usage if none are specified
+    help-all        prints usage of all command.
+    test            runs a test on the program
+    payslips        prints payslips
+    payments        view payments
+    add-payment     add a new payment
+EOF
+}
 
-    payment
+payslip-usage () {
+    cat << EOF
+${txtbld}SYNOPIS${txtrst}
+    Displays payslips
 
-    overview
+${txtbld}USAGE${txtrst}
+    ./budget payslips
 
-    estimate
+${txtbld}PARAMETERS${txtrst}
+    --count             defines the amount of payslips that will be printed
+    -- average | -a     tells the program to print the average income from the printed payslips.
+EOF
+}
 
+payments-usage () {
+    cat << EOF
 
 EOF
 }
 
+add-payment-test () {
+    cat << EOF
+
+EOF
+}
+
+test-usage () {
+    cat << EOF
+
+EOF
+}
+
+
+
 parse () {
     case $1 in
-        -help|-h|"")
+        --help|-h|"")
             usage ;;
-        -test|-t)
+        test)
             lein test ;;
         payslip)
             lein exec -p ./src/budget/core.clj ;;
-        estimate)
-            lein exec -p ./src/budget/core.clj ;;
-        overview|-o)
-            lein exec -p ./src/budget/core.clj ;;
         payment)
             lein exec -p ./src/budget/core.clj ;;
+        add-payment)
+            lein exec -p ./src/budget/core.clj ;;
+        help-all)
+            usage
     esac
 }
 
